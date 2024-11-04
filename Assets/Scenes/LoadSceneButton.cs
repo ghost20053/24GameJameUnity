fe655e030869441571a804318f9d18743872a590
Holden <holdenjp1@outlook.com>
2024-11-05 02:37:55 +1000

    Game Shell

---
+++ Assets/Scenes/LoadSceneButton.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+//This script does not do anything by itself. Instead, a UI Button should be used to call its QuitApplication functions.
+public class LoadSceneButton : MonoBehaviour
+{
+    public void LoadScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
