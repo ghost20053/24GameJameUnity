a0d9c5ce67fec675963d181a822c15bfad810444
Holden <holdenjp1@outlook.com>
2024-11-05 04:23:58 +1000

    Updated Exit

--- Assets/Custom Assets/Scripts/Escape.cs
+++ Assets/Custom Assets/Scripts/Escape.cs
@@ -5,7 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class Escape : MonoBehaviour
 {
-    public GameObject player;
     // Start is called before the first frame update
     private void OnCollisionEnter(Collision collision)
     {
