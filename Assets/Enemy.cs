29a7da36f38f7879e316651b2ed610e0552fe802
Holden <holdenjp1@outlook.com>
2024-11-04 14:28:54 +1000

    update

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -14,7 +14,7 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 direction = (target.position - transform.position).normalized;
-        transform.Translate((direction * moveSpeed) * Time.deltaTime);
+        //Vector3 direction = (target.position - transform.position).normalized;
+        //transform.Translate((direction * moveSpeed) * Time.deltaTime);
     }
 }
